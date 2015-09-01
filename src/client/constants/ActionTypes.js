8e86f4fe738c63d1d815716fad2cb7b335e0714e
Yu Wu <chaconnewu@gmail.com>
2015-09-01 13:52:22 -0700

    Add action types

---
+++ src/client/constants/ActionTypes.js
@@ -0,0 +1,4 @@
+export constant ADD_PROCON = 'ADD_PROCON';
+export constant DELETE_PROCON = 'DELETE_PROCON';
+export constant ADD_SUPPORT = 'ADD_SUPPORT';
+export constant DELETE_SUPPORT = 'DELETE_SUPPORT';
